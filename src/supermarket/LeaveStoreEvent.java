3b14a6afe38f382eadc124deb294b95e59f27af8
Aleksander Dokken <68429376+MagicDocJr@users.noreply.github.com>
2022-03-06 13:58:36 +0100

    Create LeaveStoreEvent.java

---
+++ src/supermarket/LeaveStoreEvent.java
@@ -0,0 +1,27 @@
+package supermarket;
+
+import eventsim.Event;
+
+public class LeaveStoreEvent extends Event {
+    Customer customer;
+    Checkout checkout;
+
+    public LeaveStoreEvent(Customer customer) {
+        super(customer.leaveTime);
+        this.customer = customer;
+        checkout = customer.checkout;
+    }
+
+    @Override
+    public Event happen() {
+        checkout.lastCustomerLeaveTime = customer.leaveTime;
+        checkout.customerQueue.remove(customer);
+        return null;
+    }
+
+    @Override
+    public String toString() {
+        return customer + " leaves the store";
+    }
+}
+

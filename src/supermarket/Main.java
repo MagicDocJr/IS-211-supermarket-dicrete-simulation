25acb6f256b03e89a9d60d4faf17943b68568cfd
Aleksander Dokken <68429376+MagicDocJr@users.noreply.github.com>
2022-03-06 13:58:54 +0100

    Create Main.java

---
+++ src/supermarket/Main.java
@@ -0,0 +1,15 @@
+package supermarket;
+
+public class Main {
+    public static void main(String[] args) {
+        SuperMarket superMarket = new SuperMarket();
+        superMarket.startSim();
+        System.out.println("\nStatistics: ");
+        for(Checkout checkout : superMarket.checkouts){
+            System.out.println(checkout + ": ");
+            System.out.println("Customers spent on average " + checkout.customerAverageQueueTime() + " time units waiting in queue");
+            System.out.println("The average queue size at " + " was " + checkout.checkoutAverageQueueSize() + " customers \n");
+        }
+    }
+
+}
